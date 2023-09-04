b2e06fffe9dcc2db14a28b82ced10fcb623a2d5d
tamer shamma <tamer.shma19@gmail.com>
2023-09-04 20:57:28 +0300

    his is the basi setup of the project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit{
+
+  recipes : Recipe[] = [
+    new Recipe('First Recipe','This a test description','https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'),
+    new Recipe('First Recipe','This a test description','https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'),
+    new Recipe('First Recipe','This a test description','https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg')
+  ];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients("Apple",5),
+    new Ingredients("tomato",10),
+  ];
+  
+ngOnInit(): void {
+}
+
+}
